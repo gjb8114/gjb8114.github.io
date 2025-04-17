5fd17045cdd99a4d4ce256f5f361e2c05d0b9408
Canmor Lam <canmor.lam@gmail.com>
2025-04-17 10:46:37 +0800

    feat: add Google verification metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'GJB 8114-2013 clang-tidy 检查插件',
   description: 'clang-tidy-gjb8114 功能与准则文档',
+  verification: {
+    google: "QZS-fvpVnIyam1IS3CyZcAwn1SIqGGudssXJVc4IqKc"
+  }
 };
 
 export default function RootLayout({
